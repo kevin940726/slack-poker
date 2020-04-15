5589f0e62027a4f26219cbb5ef7e2856572d1a8c
Kai Hao <kevin830726@gmail.com>
2020-04-15 10:33:35 +0800

    Fix health type

--- api/health.ts
+++ api/health.ts
@@ -1,5 +1,5 @@
 import { NowRequest, NowResponse } from '@now/node';
 
-export default async (req: NowRequest, res: NowResponse) => {
+export default async (_req: NowRequest, res: NowResponse) => {
   res.send('OK');
 };
